9d229b25c3d6d6f2cfca16cbdadb9c857a8004d6
Matthias Schabel <boost@schabel-family.org>
2007-03-31 01:19:45 +0000

    headers with static const dimensionless definitions
    [SVN r3859]

---
+++ include/boost/units/systems/cgs/dimensionless.hpp
@@ -0,0 +1,30 @@
+// mcs::units - A C++ library for zero-overhead dimensional analysis and 
+// unit/quantity manipulation and conversion
+//
+// Copyright (C) 2003-2007 Matthias Christian Schabel
+// Copyright (C) 2007 Steven Watanabe
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_UNITS_CGS_DIMENSIONLESS_HPP
+#define BOOST_UNITS_CGS_DIMENSIONLESS_HPP
+
+#include <boost/units/systems/cgs/base.hpp>
+
+namespace boost {
+
+namespace units { 
+
+namespace CGS {
+
+BOOST_UNITS_STATIC_CONSTANT(cgs_dimensionless,dimensionless);
+
+} // namespace CGS
+
+} // namespace units
+
+} // namespace boost
+
+#endif // BOOST_UNITS_CGS_DIMENSIONLESS_HPP
