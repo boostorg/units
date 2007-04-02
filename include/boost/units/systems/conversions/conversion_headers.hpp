92c3ccbf9c500ba9b7d36f66be5e032bf1846369
Matthias Schabel <boost@schabel-family.org>
2007-04-02 21:24:44 +0000

    temperature conversions
    [SVN r3864]

--- include/boost/units/systems/conversions/conversion_headers.hpp
+++ include/boost/units/systems/conversions/conversion_headers.hpp
@@ -16,6 +16,8 @@
     #include <boost/units/systems/conversions/convert_si_to_cgs.hpp>
 #endif
 
+// angle conversions
+
 #if defined(BOOST_UNITS_DEGREES_HPP) && defined(BOOST_UNITS_GRADIANS_HPP)
     #include <boost/units/systems/conversions/convert_degrees_to_gradians.hpp>
     #include <boost/units/systems/conversions/convert_gradians_to_degrees.hpp>
@@ -29,4 +31,21 @@
 #if defined(BOOST_UNITS_GRADIANS_HPP) && defined(BOOST_UNITS_RADIANS_HPP)
     #include <boost/units/systems/conversions/convert_gradians_to_radians.hpp>
     #include <boost/units/systems/conversions/convert_radians_to_gradians.hpp>
+#endif
+
+// temperature conversions
+
+#if defined(BOOST_UNITS_CELSIUS_HPP) && defined(BOOST_UNITS_FAHRENHEIT_HPP)
+    #include <boost/units/systems/conversions/convert_celsius_to_fahrenheit.hpp>
+    #include <boost/units/systems/conversions/convert_fahrenheit_to_celsius.hpp>
+#endif
+
+#if defined(BOOST_UNITS_CELSIUS_HPP) && defined(BOOST_UNITS_SI_TEMPERATURE_HPP)
+    #include <boost/units/systems/conversions/convert_celsius_to_kelvin.hpp>
+    #include <boost/units/systems/conversions/convert_kelvin_to_celsius.hpp>
+#endif
+
+#if defined(BOOST_UNITS_FAHRENHEIT_HPP) && defined(BOOST_UNITS_SI_TEMPERATURE_HPP)
+    #include <boost/units/systems/conversions/convert_fahrenheit_to_kelvin.hpp>
+    #include <boost/units/systems/conversions/convert_kelvin_to_fahrenheit.hpp>
 #endif
