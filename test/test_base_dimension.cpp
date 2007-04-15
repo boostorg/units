fcd4479948273bc097f0cd8ab8f1890f957985e6
Steven Watanabe <steven@providere-consulting.com>
2007-04-15 01:40:40 +0000

    make sure that we can use base_dimension reliably
    [SVN r3963]

---
+++ test/test_base_dimension.cpp
@@ -0,0 +1,31 @@
+// mcs::units - A C++ library for zero-overhead dimensional analysis and 
+// unit/quantity manipulation and conversion
+//
+// Copyright (C) 2003-2007 Matthias Christian Schabel
+// Copyright (C) 2007 Steven Watanabe
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+/** 
+\file
+    
+\brief test_base_dimension.cpp
+
+\detailed
+Test base_dimension class.
+
+Output:
+@verbatim
+@endverbatim
+**/
+
+#include <boost/units/base_dimension.hpp>
+
+struct dimension : boost::units::base_dimension<dimension, 1> {
+    typedef boost::units::base_dimension<dimension, 1> base;
+};
+
+int main() {
+}
