f72230ee1a12dea2241d13a42b7107a5f1492e10
Matthias Schabel <boost@schabel-family.org>
2007-05-31 18:47:05 +0000

    missing #include
    [SVN r4388]

--- include/boost/units/systems/cgs/area.hpp
+++ include/boost/units/systems/cgs/area.hpp
@@ -12,6 +12,7 @@
 #define BOOST_UNITS_CGS_AREA_HPP
 
 #include <boost/units/systems/cgs/base.hpp>
+#include <boost/units/systems/derived_dimensions/area_derived_dimension.hpp>
 
 namespace boost {
 
