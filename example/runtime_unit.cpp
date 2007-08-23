ec80e73cad567cb3da08facb40fda77075ab3c43
Steven Watanabe <steven@providere-consulting.com>
2007-08-23 18:03:38 +0000

    Finish runtime_unit.cpp example
    [SVN r38866]

--- example/runtime_unit.cpp
+++ example/runtime_unit.cpp
@@ -12,19 +12,26 @@
 #include <iostream>
 #include <boost/lexical_cast.hpp>
 #include <boost/units/quantity.hpp>
+#include <boost/units/cmath.hpp>
 #include <boost/units/systems/si/length.hpp>
 #include <boost/units/systems/base_units.hpp>
 
+//[runtime_unit_snippet_1
+
 namespace {
 
-std::map<std::string, double> known_units;
+std::map<std::string, boost::units::quantity<boost::units::SI::length> > known_units;
 
 }
 
+boost::units::quantity<boost::units::SI::length> calculate(const boost::units::quantity<boost::units::SI::length>& t) {
+    return(boost::units::hypot(t, 2.0 * boost::units::SI::meters));
+}
+
 int main() {
-    known_units["meter"] = 1.0;
-    known_units["centimeter"] = .01;
-    known_units["foot"] = conversion_factor(boost::units::foot_base_unit::unit_type(), boost::units::SI::meter);
+    known_units["meter"] = 1.0 * boost::units::SI::meters;
+    known_units["centimeter"] = .01 * boost::units::SI::meters;;
+    known_units["foot"] = conversion_factor(boost::units::foot_base_unit::unit_type(), boost::units::SI::meter) * boost::units::SI::meters;;
     std::string output_type("meter");
     std::string input;
     while(std::cin >> input) {
@@ -32,7 +39,7 @@ int main() {
         else if(input == "help") {
             std::cout << "type \"exit\" to exit\n"
                 "type \"return 'unit'\" to set the return units\n"
-                "type \"'number' 'unit'\" to convert a quantity to the return units" << std::endl;
+                "type \"'number' 'unit'\" to do a simple calculation" << std::endl;
         } else if(input == "return") {
             if(std::cin >> input) {
                 if(known_units.find(input) != known_units.end()) {
@@ -47,7 +54,7 @@ int main() {
                 double value = boost::lexical_cast<double>(input);
                 if(std::cin >> input) {
                     if(known_units.find(input) != known_units.end()) {
-                        std::cout << (value * known_units[input] / known_units[output_type]) << ' ' << output_type << std::endl;
+                        std::cout << static_cast<double>(calculate(value * known_units[input]) / known_units[output_type]) << ' ' << output_type << std::endl;
                     } else {
                         std::cout << "Unknown unit \"" << input << "\"" << std::endl;
                     }
@@ -58,3 +65,5 @@ int main() {
         }
     }
 }
+
+//]
