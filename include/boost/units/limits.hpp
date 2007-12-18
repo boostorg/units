6a702ee02de9ce208e51fbd4ad8ae15f2bcdee7f
Matthias Schabel <boost@schabel-family.org>
2007-12-18 18:53:48 +0000

    add include guards
    [SVN r42143]

--- include/boost/units/limits.hpp
+++ include/boost/units/limits.hpp
@@ -7,6 +7,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
+#ifndef BOOST_UNITS_LIMITS_HPP
+#define BOOST_UNITS_LIMITS_HPP
 
 #include <limits>
 
@@ -52,3 +54,5 @@ class numeric_limits< ::boost::units::quantity<Unit, T> >
 };
 
 }
+
+#endif // BOOST_UNITS_LIMITS_HPP
