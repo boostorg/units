6a4f9afb1f3b76c7c00ffa756bdb17ca93dd700f
Steven Watanabe <steven@providere-consulting.com>
2008-05-14 03:46:32 +0000

    Added new line at end of file
    [SVN r45351]

--- include/boost/units/limits.hpp
+++ include/boost/units/limits.hpp
@@ -2,7 +2,7 @@
 // unit/quantity manipulation and conversion
 //
 // Copyright (C) 2003-2007 Matthias Christian Schabel
-// Copyright (C) 2007 Steven Watanabe
+// Copyright (C) 2007-2008 Steven Watanabe
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
