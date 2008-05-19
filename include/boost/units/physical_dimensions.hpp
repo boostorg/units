beff3c7dd8d6c546c53d9d95932139a0f925e5a9
Matthias Schabel <boost@schabel-family.org>
2008-05-19 00:55:35 +0000

    *** MAJOR ***
    potentially breaking changes : rename SI/CGS namespaces to lowercase
    remove some non-SI units (mostly obscure)
    
    move base_units into boost/units
    move physical_dimensions into boost/units
    improved base_units for non-SI 
    
    [SVN r45512]

---
+++ include/boost/units/physical_dimensions.hpp
@@ -0,0 +1,52 @@
+// Boost.Units - A C++ library for zero-overhead dimensional analysis and 
+// unit/quantity manipulation and conversion
+//
+// Copyright (C) 2003-2008 Matthias Christian Schabel
+// Copyright (C) 2007-2008 Steven Watanabe
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_UNITS_PHYSICAL_UNITS_HPP
+#define BOOST_UNITS_PHYSICAL_UNITS_HPP
+
+/// \file Includes all of the physical_dimension headers.
+
+#include <boost/units/physical_dimensions/amount.hpp>
+#include <boost/units/physical_dimensions/current.hpp>
+#include <boost/units/physical_dimensions/length.hpp>
+#include <boost/units/physical_dimensions/luminous_intensity.hpp>
+#include <boost/units/physical_dimensions/mass.hpp>
+#include <boost/units/physical_dimensions/plane_angle.hpp>
+#include <boost/units/physical_dimensions/solid_angle.hpp>
+#include <boost/units/physical_dimensions/temperature.hpp>
+#include <boost/units/physical_dimensions/time.hpp>
+
+#include <boost/units/physical_dimensions/absorbed_dose.hpp>
+#include <boost/units/physical_dimensions/acceleration.hpp>
+#include <boost/units/physical_dimensions/action.hpp>
+#include <boost/units/physical_dimensions/activity.hpp>
+#include <boost/units/physical_dimensions/angular_velocity.hpp>
+#include <boost/units/physical_dimensions/area.hpp>
+#include <boost/units/physical_dimensions/dose_equivalent.hpp>
+#include <boost/units/physical_dimensions/dynamic_viscosity.hpp>
+#include <boost/units/physical_dimensions/energy.hpp>
+#include <boost/units/physical_dimensions/force.hpp>
+#include <boost/units/physical_dimensions/frequency.hpp>
+#include <boost/units/physical_dimensions/illuminance.hpp>
+#include <boost/units/physical_dimensions/kinematic_viscosity.hpp>
+#include <boost/units/physical_dimensions/luminance.hpp>
+#include <boost/units/physical_dimensions/luminous_flux.hpp>
+#include <boost/units/physical_dimensions/mass_density.hpp>
+#include <boost/units/physical_dimensions/momentum.hpp>
+#include <boost/units/physical_dimensions/power.hpp>
+#include <boost/units/physical_dimensions/pressure.hpp>
+#include <boost/units/physical_dimensions/specific_volume.hpp>
+#include <boost/units/physical_dimensions/stress.hpp>
+#include <boost/units/physical_dimensions/surface_density.hpp>
+#include <boost/units/physical_dimensions/velocity.hpp>
+#include <boost/units/physical_dimensions/volume.hpp>
+#include <boost/units/physical_dimensions/wavenumber.hpp>
+
+#endif // BOOST_UNITS_PHYSICAL_UNITS_HPP
