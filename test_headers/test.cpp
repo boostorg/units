92c7e688d71d198058349fea483cbb91996ace7d
Steven Watanabe <steven@providere-consulting.com>
2008-05-22 13:28:33 +0000

    Fixed copyright/license
    [SVN r45652]

--- test_headers/test.cpp
+++ test_headers/test.cpp
@@ -1,7 +1,8 @@
-// test.cpp
+// Boost.Units - A C++ library for zero-overhead dimensional analysis and 
+// unit/quantity manipulation and conversion
 //
-// Copyright (c) 2007-2008
-// Steven Watanabe
+// Copyright (C) 2003-2008 Matthias Christian Schabel
+// Copyright (C) 2007-2008 Steven Watanabe
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
