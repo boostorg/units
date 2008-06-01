5222eef795cba4a2c3e5e4bb281f4bd944dfd39b
Matthias Schabel <boost@schabel-family.org>
2008-06-01 08:31:29 +0000

    something funky going on here...need to fix
    [SVN r46006]

--- example/runtime_unit.cpp
+++ example/runtime_unit.cpp
@@ -27,50 +27,89 @@ std::map<std::string, quantity<si::length> > known_units;
 
 }
 
-quantity<si::length> calculate(const quantity<si::length>& t) {
+quantity<si::length> calculate(const quantity<si::length>& t) 
+{
     return(boost::units::hypot(t, 2.0 * si::meters));
 }
 
-int main() {
+// enter "return meter", then 3.4 centimeter 
+// -> gives 2.00029 meter as output?
+
+int main() 
+{
     known_units["meter"] = 1.0 * si::meters;
     known_units["centimeter"] = .01 * si::meters;;
     known_units["foot"] =
         conversion_factor(foot_base_unit::unit_type(), si::meter) * si::meter;
+        
     std::string output_type("meter");
     std::string input;
-    while((std::cout << ">") && (std::cin >> input)) {
-        if(!input.empty() && input[0] == '#') std::getline(std::cin, input);
-        else if(input == "exit") break;
-        else if(input == "help") {
+    
+    while((std::cout << ">") && (std::cin >> input)) 
+    {
+        if(!input.empty() && input[0] == '#') 
+        {
+            std::getline(std::cin, input);
+        }
+        else if(input == "exit") 
+        {
+            break;
+        }
+        else if(input == "help") 
+        {
             std::cout << "type \"exit\" to exit\n"
                 "type \"return 'unit'\" to set the return units\n"
                 "type \"'number' 'unit'\" to do a simple calculation"
                 << std::endl;
-        } else if(input == "return") {
-            if(std::cin >> input) {
-                if(known_units.find(input) != known_units.end()) {
+        } 
+        else if(input == "return") 
+        {
+            if(std::cin >> input) 
+            {
+                if(known_units.find(input) != known_units.end()) 
+                {
                     output_type = input;
                     std::cout << "Done." << std::endl;
-                } else {
+                } 
+                else 
+                {
                     std::cout << "Unknown unit \"" << input << "\""
                          << std::endl;
                 }
-            } else break;
-        } else {
-            try {
+            } 
+            else 
+            {
+                break;
+            }
+        } 
+        else 
+        {
+            try 
+            {
                 double value = boost::lexical_cast<double>(input);
-                if(std::cin >> input) {
-                    if(known_units.find(input) != known_units.end()) {
+                
+                if(std::cin >> input) 
+                {
+                    if(known_units.find(input) != known_units.end()) 
+                    {
                         std::cout << static_cast<double>(
                             calculate(value * known_units[input]) /
                             known_units[output_type])
                             << ' ' << output_type << std::endl;
-                    } else {
+                    } 
+                    else 
+                    {
                         std::cout << "Unknown unit \"" << input << "\""
                             << std::endl;
                     }
-                } else break;
-            } catch(...) {
+                } 
+                else 
+                {
+                    break;
+                }
+            } 
+            catch(...) 
+            {
                 std::cout << "Input error" << std::endl;
             }
         }
