99ddca6dbbb7f714c8825f208986b58cb92d2c04
E Kawashima <e-kwsm@users.noreply.github.com>
2018-07-21 20:00:28 +0900

    Update to 2014 CODATA recommended values

--- include/boost/units/systems/si/codata/universal_constants.hpp
+++ include/boost/units/systems/si/codata/universal_constants.hpp
@@ -1,4 +1,4 @@
-// Boost.Units - A C++ library for zero-overhead dimensional analysis and 
+// Boost.Units - A C++ library for zero-overhead dimensional analysis and
 // unit/quantity manipulation and conversion
 //
 // Copyright (C) 2003-2008 Matthias Christian Schabel
@@ -29,20 +29,17 @@
 #include <boost/units/systems/si/codata/typedefs.hpp>
 
 /// \file
-/// CODATA recommended values of fundamental universal constants
-/// using CODATA 2006 values as of 2007/03/30
+/// 2014 CODATA recommended values of fundamental universal constants as of 2018/07/21
 
 namespace boost {
 
-namespace units { 
+namespace units {
 
 namespace si {
-                            
+
 namespace constants {
 
 namespace codata {
-
-/// CODATA recommended values of the fundamental physical constants: NIST SP 961
 
 // UNIVERSAL
 /// speed of light
@@ -54,23 +51,23 @@ BOOST_UNITS_PHYSICAL_CONSTANT(epsilon_0,quantity<capacitance_over_length>,8.8541
 /// characteristic impedance of vacuum
 BOOST_UNITS_PHYSICAL_CONSTANT(Z_0,quantity<resistance>,376.7303134617706554681984004203193082686*ohm,0.0*ohm);
 /// Newtonian constant of gravitation
-BOOST_UNITS_PHYSICAL_CONSTANT(G,quantity<volume_over_mass_time_squared>,6.67428e-11*cubic_meters/kilogram/second/second,6.7e-15*cubic_meters/kilogram/second/second);
+BOOST_UNITS_PHYSICAL_CONSTANT(G,quantity<volume_over_mass_time_squared>,6.67408e-11*cubic_meters/kilogram/second/second,3.1e-15*cubic_meters/kilogram/second/second);
 /// Planck constant
-BOOST_UNITS_PHYSICAL_CONSTANT(h,quantity<energy_time>,6.62606896e-34*joule*seconds,3.3e-41*joule*seconds);
+BOOST_UNITS_PHYSICAL_CONSTANT(h,quantity<energy_time>,6.626070040e-34*joule*seconds,8.1e-42*joule*seconds);
 /// Dirac constant
-BOOST_UNITS_PHYSICAL_CONSTANT(hbar,quantity<energy_time>,1.054571628e-34*joule*seconds,5.3e-42*joule*seconds);
+BOOST_UNITS_PHYSICAL_CONSTANT(hbar,quantity<energy_time>,1.054571800e-34*joule*seconds,1.3e-42*joule*seconds);
 /// Planck mass
-BOOST_UNITS_PHYSICAL_CONSTANT(m_P,quantity<mass>,2.17644e-8*kilograms,1.1e-12*kilograms);
+BOOST_UNITS_PHYSICAL_CONSTANT(m_P,quantity<mass>,2.176470e-8*kilograms,5.1e-13*kilograms);
 /// Planck temperature
-BOOST_UNITS_PHYSICAL_CONSTANT(T_P,quantity<temperature>,1.416785e32*kelvin,7.1e27*kelvin);
+BOOST_UNITS_PHYSICAL_CONSTANT(T_P,quantity<temperature>,1.416808e32*kelvin,3.3e27*kelvin);
 /// Planck length
-BOOST_UNITS_PHYSICAL_CONSTANT(l_P,quantity<length>,1.616252e-35*meters,8.1e-40*meters);
+BOOST_UNITS_PHYSICAL_CONSTANT(l_P,quantity<length>,1.616229e-35*meters,3.8e-40*meters);
 /// Planck time
-BOOST_UNITS_PHYSICAL_CONSTANT(t_P,quantity<time>,5.39124e-44*seconds,2.7e-48*seconds);
+BOOST_UNITS_PHYSICAL_CONSTANT(t_P,quantity<time>,5.39116e-44*seconds,1.3e-48*seconds);
 
 } // namespace codata
 
-} // namespace constants    
+} // namespace constants
 
 } // namespace si
 
