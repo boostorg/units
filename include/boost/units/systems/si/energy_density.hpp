b6845b151b7b3df1a36b3f81871cb44ac3df0f7a
E Kawashima <e-kwsm@users.noreply.github.com>
2018-07-22 09:47:59 +0900

    add missing SI quantities
    - energy_density
    - heat_capacity
    - luminance
    - molar_energy
    - molar_heat_capacity
    - specific_energy
    - specific_heat_capacity
    - specific_volume
    - stress
    - thermal_conductivity

---
+++ include/boost/units/systems/si/energy_density.hpp
@@ -0,0 +1,30 @@
+// Boost.Units - A C++ library for zero-overhead dimensional analysis and
+// unit/quantity manipulation and conversion
+//
+// Copyright (C) 2018 Eisuke Kawashima
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_UNITS_SI_ENERGY_DENSITY_HPP
+#define BOOST_UNITS_SI_ENERGY_DENSITY_HPP
+
+#include <boost/units/systems/si/base.hpp>
+#include <boost/units/physical_dimensions/energy_density.hpp>
+
+namespace boost {
+
+namespace units {
+
+namespace si {
+
+typedef unit<energy_density_dimension,si::system> energy_density;
+
+} // namespace si
+
+} // namespace units
+
+} // namespace boost
+
+#endif // BOOST_UNITS_SI_ENERGY_DENSITY_HPP
